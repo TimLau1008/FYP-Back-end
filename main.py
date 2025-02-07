16f37806918473b1fac56e324dbe90fdfab516f7
TimLau1008 <laukaihing1008@gmail.com>
2025-02-07 21:45:15 +0800

    minor changes

--- main.py
+++ main.py
@@ -8,6 +8,3 @@ transcriptor = PianoTranscription(device='cpu')    # 'cuda' | 'cpu'
 
 # Transcribe and write out to MIDI file
 transcribed_dict = transcriptor.transcribe(audio, 'example.mid')
-
-#git config --global user.email "you@example.com"
-#git config --global user.name "Your Name"
